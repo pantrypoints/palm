bbc2d567e39ab9e6b438194226f344e86fbe8738
“pantrypoints” <pantrypoints@gmail.com>
2026-04-18 23:37:20 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
+// import { sveltekit } from '@sveltejs/kit/vite';
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//   plugins: [sveltekit()]
+// });
+
+
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  ssr: {
+    // This tells Vite to let the Cloudflare runtime handle these modules
+    external: ['node:assert', 'node:crypto', 'node:path', 'node:url']
+  }
 });
