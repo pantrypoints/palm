0088df43ba36114d944bc03556f937af1c18d031
“pantrypoints” <pantrypoints@gmail.com>
2026-04-18 16:56:32 +0800

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-cloudflare'; 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
+    // adapter-cloudflare doesn't usually need extra config for basic builds
     adapter: adapter()
   }
 };
 
 export default config;
+
